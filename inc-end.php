c75e8e8282b1cf7c0e74fc9a15a9fabde0155927
Aleksander Støle <aleksander.stole@outlook.com>
2025-06-19 11:54:19 +0200

    AdSense step 2
    AdSense step 2

--- inc-end.php
+++ inc-end.php
@@ -1,3 +1,16 @@
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9553054176028249"
+     crossorigin="anonymous"></script>
+<!-- astole.me footer -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-9553054176028249"
+     data-ad-slot="4814454732"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+
 <footer>
 	<div>
 		<span id="copyright" class="form_buttons_copyright">&copy; <?php echo date("Y") ?> Aleksander Støle<div><a href="mailto:aleksander.stole@outlook.com">aleksander.stole@outlook.com</a></div></span>
